30c20a0e0fa73de5b4384338f3b3436b5611cb12
EduardoPacheco12 <eduardooliveirapacheco131@gmail.com>
2022-09-21 15:42:29 -0300

    feat: add the recommendations route post tests

---
+++ back-end/tests/factories/songFactory.ts
@@ -0,0 +1,11 @@
+import { faker } from "@faker-js/faker";
+import { createYoutubeLink } from "./youtubeLinkFactory";
+
+export async function createSong() {
+	const song = {
+		name: faker.music.songName(),
+		youtubeLink: await createYoutubeLink()
+	}
+
+	return song;
+}
---
+++ back-end/tests/recommendation.test.ts
@@ -0,0 +1,68 @@
+import supertest from "supertest";
+import app from "../src/app.js";
+import { createSong } from "./factories/songFactory.js";
+import { prisma } from "../src/database.js";
+
+const agent = supertest(app);
+
+beforeEach(async () => {
+	await prisma.$executeRaw`TRUNCATE TABLE "recommendations" RESTART IDENTITY`;
+});
+
+describe("Test POST /recommendations", () => {
+	it("Should return 201 if registered a song in the correct format", async () => {
+		const song = await createSong();
+		
+		const response = await agent.post("/recommendations").send(song);
+
+		expect(response.status).toBe(201);
+	});
+
+	it("Should return 409 if the song has already been registered", async () => {
+		const song = await createSong();
+
+		await prisma.recommendation.create({
+			data: song
+		})
+
+		const response = await agent.post("/recommendations").send(song);
+
+		expect(response.status).toBe(409);
+	});
+
+	it("Should return 422 if the song name isn't a string or is empty", async () => {
+		const song = await createSong();
+
+		song.name = "";
+		
+		const response = await agent.post("/recommendations").send(song);
+
+		expect(response.status).toBe(422);
+	});
+
+	it("Should return 422 if the Youtube's link isn't a YouTube's url", async () => {
+		const song = await createSong();
+
+		song.youtubeLink = "aojgftajfpjkdka";
+		
+		const response = await agent.post("/recommendations").send(song);
+
+		expect(response.status).toBe(422);
+	});
+});
+
+describe("Test POST /recommendations/:id/upvote", () => {});
+
+describe("Test POST /recommendations/:id/downvote", () => {});
+
+describe("Test GET /recommendations", () => {});
+
+describe("Test GET /recommendations/:id", () => {});
+
+describe("Test GET /recommendations/random", () => {});
+
+describe("Test GET /recommendations/top/:amount", () => {});
+
+afterAll(async () => {
+	await prisma.$disconnect();
+})
